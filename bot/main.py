3656a2b7d6add50dbd5fcc05edbb3f48476a1a21
lsglucas <lsglucas@protonmail.com>
2020-09-05 15:57:50 -0300

    Bugged Version

--- bot/main.py
+++ bot/main.py
@@ -4,30 +4,43 @@ from itertools import cycle
 from commandslist import *
 from secret import Bot_token
 
+
 Prefix = 'm-'
 client = commands.Bot(command_prefix=Prefix)
 status = cycle(['Procurando atualizações...', 'Olhando para o Moodle', 'Descobrindo tarefas', 'Destruindo a casa do Jamil'])
+PATH = 'C:\\Users\\lukep\\Projects\\Discord Bot\\DiscordMackBot\\template.csv'
+
 
 @client.event
 async def on_ready():
     change_status.start()
     print('We have logged in as {0.user}'.format(client))
 
+
 @tasks.loop(seconds=10)
 async def change_status():
+    print("next status")
     await client.change_presence(activity=discord.Game(next(status)))
+
 
 @client.command()
 async def clear(ctx, amount=10):
+    print("clear command")
     await ctx.channel.purge(limit=amount)
 
+
 @client.command()
-async def get_data():
-    pass
+async def print_data():
+    print('print_data')
+    with open(PATH, "r") as data:
+        for line in range(1, len(data)):
+            await channel.send(f"# {line}")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
 
 client.run(Bot_token)
