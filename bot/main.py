f1afed3a36791ed07292ffee914f09ea88a6cd75
lsglucas <lsglucas@protonmail.com>
2020-09-05 14:41:20 -0300

    csv added and started get data function

--- bot/main.py
+++ bot/main.py
@@ -8,22 +8,22 @@ Prefix = 'm-'
 client = commands.Bot(command_prefix=Prefix)
 status = cycle(['Procurando atualizações...', 'Olhando para o Moodle', 'Descobrindo tarefas', 'Destruindo a casa do Jamil'])
 
-@client.command()
-async def clear(ctx, amount=10):
-    await ctx.channel.purge(limit=amount)
-
-
 @client.event
 async def on_ready():
     change_status.start()
     print('We have logged in as {0.user}'.format(client))
 
-
-
 @tasks.loop(seconds=10)
 async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
+@client.command()
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def get_data():
+    pass
 
 @client.event
 async def on_message(message):
