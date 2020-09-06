afc52353c5bc621be9f5def015d0a8a6df0b55fb
Lucas <lsglucas@protonmail.com>
2020-09-06 01:39:25 -0300

    Changed preffix and the name of the commands. Comments added

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,7 @@ import pandas as pd
 # import bitlyshortener
 
 #short = bitlyshortener.Shortener(tokens=bitly_token, vanitize=True)
-Prefix = 'm-'
+Prefix = 'mack '
 client = commands.Bot(command_prefix=Prefix)
 status = cycle(['Estudando...', 'Navegando no Moodle', 'Descobrindo tarefas', 'Dominando o mundo', 'Reduzindo as suas faltas', 'Calculando as suas médias'])
 PATH = r'C:\Users\lukep\Documents\Projects\Discord Bot\DiscordMackBot\csvfiles\events.csv'
@@ -18,26 +18,26 @@ async def on_ready():
     change_status.start()
     print('We have logged in as {0.user}'.format(client))
 
-
+#Discord live status that rotate from the list
 @tasks.loop(seconds=300)
 async def change_status():
     print("next status")
     await client.change_presence(activity=discord.Game(next(status)))
 
-
+#Clear the previous line for x amout of times
 @client.command()
 async def clear(ctx, amount=3):
     print("clear command")
     await ctx.channel.purge(limit=amount)
 
-
+#Command to get the assignments from the csv and send it embeded to the text chat
 @client.command()
-async def tarefas(ctx):
+async def assignments(ctx):
     database = pd.read_csv(PATH, header=None )
     
     # urls = pd.DataFrame(database, columns=['deadline'])
     # print(urls, type(urls))
-    for i in range(len(database)):# numero de linhas do csv
+    for i in range(len(database)):# amount of rows of the csv
         fullname = database.iat[i,0]
         name = database.iat[i,1]
         description = database.iat[i,2]
@@ -48,6 +48,8 @@ async def tarefas(ctx):
         # print(link, url)
         # print(database)
 
+
+        #Creating a style for the message 
         embed=discord.Embed(title=modulename, color=0xcc0000)
         embed.set_thumbnail(url="https://logodownload.org/wp-content/uploads/2017/09/mackenzie-logo-3.png")
         embed.add_field(name="Matéria", value=fullname, inline=True)
