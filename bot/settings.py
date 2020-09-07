cef105c39cc20ce048a5358e7f3b1374fa0b3f61
Lucas <lsglucas@protonmail.com>
2020-09-07 02:59:05 -0300

    Added comments on Settings about csv and moodle integration

--- bot/settings.py
+++ bot/settings.py
@@ -1,6 +1,8 @@
 import os
 
 # Settings file to get the path
+# The folder csvfiles and its files were deleted from the git to save our personal/scholar data
+# If you wanna use this bot to get information about your School/University you must create your own files using your token and edit the moodleapi package to fit your proposes.
 PATH_EVENTS = os.path.abspath('bot').split('bot')[0] + "csvfiles\events.csv"
 PATH_ASSIGNMENTS = os.path.abspath('bot').split('bot')[0] + "csvfiles\\assignments.csv"
 PATH_LIVECLASSES = os.path.abspath('bot').split('bot')[0] + "csvfiles\liveclasses.csv"
