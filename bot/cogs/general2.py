a714caf09a4a2f6043e330cd4291bb108c7df4ba
Lucas <lsglucas@protonmail.com>
2020-09-07 22:05:53 -0300

    Changes on General2.py

--- bot/cogs/general2.py
+++ bot/cogs/general2.py
@@ -1,5 +1,6 @@
 import discord
 from moodleapi.security import Cryptography
+from moodleapi.token import Token
 
 from discord.ext import commands, tasks
 from settings import *
@@ -24,14 +25,14 @@ class General2(commands.Cog):
                 await ctx.send(embed=embed)
                 decrypted_token = Cryptography().decrypt_message(bytes(tokens_data.iat[i,0], encoding='utf-8'))
                 embed = main_messages_style(f"Your decrypted Moodle API Token is, {decrypted_token}", "Note: You won't need to use it anywhere in this bot")
-                await ctx.send(embed=embed)
+                await ctx.author.send(embed=embed)
 
             else:
                 embed = main_messages_style("Apparently you don't have a Moodle API Token, do you want to create one? Yes/No")
                 await ctx.author.send(embed=embed)
                 answer = await self.client.wait_for('message')
 
-                if answer.content.lower() == "yes" or answer.content.lower() == "y":
+                if answer.content.lower() == "yes":
 
                     embed = main_messages_style("Type and send your Moodle username")
                     await ctx.author.send(embed=embed)
@@ -39,7 +40,10 @@ class General2(commands.Cog):
 
                     embed = main_messages_style("Type and send your Moodle passowrd")
                     await ctx.author.send(embed=embed)
-                    passowrd = await self.client.wait_for('message')
+                    password = await self.client.wait_for('message')
+
+                    Token().create(username, password, userid)
+
 
                 else:
                     embed = main_messages_style("Okay, use the **GetToken** command when you're ready to create one")
