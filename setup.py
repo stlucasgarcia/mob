79d56f866ab70d7d05b9ab5c54a56f20a5c3f606
Daniel <danielvenna2@gmail.com>
2020-09-24 08:47:08 -0300

    Created setup.py for moodleapi package

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="moodleapi",
+    version="1.0.2",
+    author="Daniel Kauffmann",
+    author_email="danielvenna2@gmail.com",
+    description="MoodleAPI Package for Bot usable",
+    long_description='Nothing here',
+    long_description_content_type="text/markdown",
+    url="https://github.com/lsglucas/DiscordMackBot",
+    packages=['moodleapi', 'moodleapi/data'],
+    install_requires=['requests', 'cryptography', 'pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
