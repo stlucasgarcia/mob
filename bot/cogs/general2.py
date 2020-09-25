faa46ba7c9e8c1781ccf42ea680030f96ee32375
Chang <61498020+ChangProg@users.noreply.github.com>
2020-09-25 08:39:42 -0300

    Commented general2.y

--- bot/cogs/general2.py
+++ bot/cogs/general2.py
@@ -15,19 +15,19 @@ class General2(Cog):
     #Creates a Caesar Cipher
     @command(name='cipher', aliases=['CIPHER', 'Cipher'])
     async def cipher(self,ctx, *text):
-        alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-        key = randint(2,25)
-        print(f"Key: {key}")
+        # Check if key value + letter index is greater than 26
         def check(index, key):
 	        if key > 26:
 		        key = key%26
 		
 	        if index + key >= 26:
 		        newKey =  index + key - 26
-		        return 0, newKey
+		        return 0, newKey # Returns new key for the letter index
 	
 	        else:
 		        return index, key
+
+        # Cipher text
         def cipher(listText, key):
             global cipherText
             cipherText= ''
@@ -35,40 +35,48 @@ class General2(Cog):
             for i in range(len(listText)):
                 
                 key = aux
-
+                
+                # Checks if the character is alphabetical
                 if not listText[i].isalpha():
                     cipherText += listText[i]
 
+                # Check if character is lower case
                 if listText[i].islower():
                     index = alphabet.index(listText[i].upper())
                     index, key = check(index, key)
                     result = alphabet[index+key]
                     cipherText += result.lower()
 
+                # Check if character is upper case
                 elif listText[i].isupper():
                     index = alphabet.index(listText[i])
                     index, key = check(index, key)
                     result = alphabet[index+key]
                     cipherText += result
     
-        
+        # Setup Alphabet and random Key
+        alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        key = randint(2,25)
+        print(f"Key: {key}")
         listText = list(text)
 
-
+        # Execute cipher for each block of text found in the text message
         for i in range(len(listText)):
-            #print(list(listText[i]))
             cipher(list(listText[i]), key)
-            
+
+        # Prints values on Console for verification  
         print(f"Text: {text}")
         print(f"Text: {listText}")
         print(f"Cipher Text: {cipherText}")
-        await ctx.channel.purge(limit=1)
 
+        # Returns cipher message
+        await ctx.channel.purge(limit=1)
         embed = main_messages_style("Cipher text",cipherText)
         await ctx.send(embed=embed)
 
+    # Creates a dice command
     @command(name='roll', aliases=['ROLL', 'Roll'])
-    async def roll(self,ctx, number=6):
+    async def roll(self,ctx, number=6): #Sets default roll number to 6
         print("Dice!")
         embed = main_messages_style("Rolling dice... 🎲")
         await ctx.send(embed=embed)
