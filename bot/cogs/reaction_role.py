c784d7001c1bfac9f49b8ee4b2ea8893e9fd7d4f
Lucas <lsglucas@protonmail.com>
2020-10-08 02:17:07 -0300

    Reaction Role feature created on DB

--- bot/cogs/reaction_role.py
+++ bot/cogs/reaction_role.py
@@ -1,8 +1,7 @@
-import discord, asyncio
+import discord, asyncio, re
 from discord.ext import tasks
 from discord.ext.commands import command, Cog
 from utilities import main_messages_style, footer, positive_emojis_list, defaultcolor
-
 
 
 class reactionRole(Cog):
@@ -12,56 +11,84 @@ class reactionRole(Cog):
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.message_id == 760608200386019338:
-            guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g : g.id == guild_id, self.client.guilds)
-            
-            if payload.emoji.name == "happy":
-                role = discord.utils.get(guild.roles, name="Coder")
+        role = None
+        guild_id = str(payload.guild_id)
 
-            if role is not None:
+        ids_data = await self.client.pg_con.fetch("SELECT menu_id FROM bot_roles WHERE guild_id = $1", guild_id)
+        ids_list = [item for i in ids_data for item in i]
+
+        if str(payload.message_id) in ids_list:
+
+            guild = self.client.get_guild(payload.guild_id)
+            emojis_data = await self.client.pg_con.fetch("SELECT emoji_name FROM bot_roles WHERE guild_id = $1", guild_id)
+            emojis_list = [item for i in emojis_data for item in i]
+
+            role_data = await self.client.pg_con.fetch("SELECT role_name FROM bot_roles WHERE guild_id = $1", guild_id)
+            role_list = [item for i in role_data for item in i]
+
+            for i in range(len(emojis_list)):
+                if ":" in emojis_list[i]:
+                    emoji = str(emojis_list[i])
+                    pattern = ":(.*?):"
+                    emoji_name_db = re.search(pattern, emoji).group(1)
+
+                if payload.emoji.name == emoji_name_db:
+                    role = discord.utils.get(guild.roles, name=role_list[i])
+                
+            if role:
                 member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
                 await member.add_roles(role)
-                print("role given")
-            
-            else:
-                print("Role not found")
+
 
 
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        if payload.message_id == 760608200386019338:
-            guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g : g.id == guild_id, self.client.guilds)
-            
-            if payload.emoji.name == "happy":
-                role = discord.utils.get(guild.roles, name="Coder")
+        role = None
+        guild_id = str(payload.guild_id)
 
-            if role is not None:
+        ids_data = await self.client.pg_con.fetch("SELECT menu_id FROM bot_roles WHERE guild_id = $1", guild_id)
+        ids_list = [item for i in ids_data for item in i]
+
+        if str(payload.message_id) in ids_list:
+            guild = self.client.get_guild(payload.guild_id)
+
+            emojis_data = await self.client.pg_con.fetch("SELECT emoji_name FROM bot_roles WHERE guild_id = $1", guild_id)
+            emojis_list = [item for i in emojis_data for item in i]
+
+            role_data = await self.client.pg_con.fetch("SELECT role_name FROM bot_roles WHERE guild_id = $1", guild_id)
+            role_list = [item for i in role_data for item in i]
+            
+            for index in range(len(emojis_list)):
+                if ":" in emojis_list[index]:
+                    emoji = str(emojis_list[index])
+                    pattern = ":(.*?):"
+                    emoji_name_db = re.search(pattern, emoji).group(1)
+                    
+
+                if payload.emoji.name == emoji_name_db:
+                    role = discord.utils.get(guild.roles, name=role_list[index])
+
+            if role:
                 member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
                 await member.remove_roles(role)
-                print("role removed")
             
-            else:
-                print("Role not found")
 
-
-    @command(name="CreateRoles", aliases=["ReactionRoles"])
+    @command(name="CreateRoles", aliases=["ReactionRoles", "CreateRole", "ReactionRole", "RoleCreate", "MenuRole", "createrole"])
     async def CreateRoles(self, ctx, amount):
 
         roles_list = []
-        
+        guild_id = str(ctx.guild.id)
         
         def check(ctx, m):
             return m.author == ctx.author
 
-        embed = main_messages_style("Reaction Role Tool", "Type the Title for the command")
+        embed = main_messages_style("Reaction Role Tool", "Type the Title for the embed message")
         await ctx.send(embed=embed)
 
         title = await self.client.wait_for('message')
+        await asyncio.sleep(1)
         await ctx.channel.purge(limit=3)
 
-        await asyncio.sleep(1)
 
         embed = main_messages_style("Reaction Role Tool", "Type the Description/Category for the roles")
         await ctx.send(embed=embed)
@@ -78,20 +105,22 @@ class reactionRole(Cog):
             await ctx.send(embed=embed)
 
             Role = await self.client.wait_for('message')
+            await asyncio.sleep(1)
             await ctx.channel.purge(limit=2)
 
             embed = main_messages_style("Reaction Role Tool", "Type the Emoji for the Role")
             await ctx.send(embed=embed)
 
             Emoji = await self.client.wait_for('message')
+            await asyncio.sleep(1)
             await ctx.channel.purge(limit=2)
 
             embed = main_messages_style("Reaction Role Tool", "Type the roles Description")
             await ctx.send(embed=embed)
 
             Description = await self.client.wait_for('message')
+            await asyncio.sleep(1)
             await ctx.channel.purge(limit=2)
-
 
 
             roles_dict = {
@@ -101,7 +130,8 @@ class reactionRole(Cog):
             }
 
             roles_list.append(roles_dict)
-        
+
+
 
         embed = discord.Embed(title=title.content, description=main_description.content, color=defaultcolor)
         embed.set_author(name="Reaction Role")
@@ -110,13 +140,19 @@ class reactionRole(Cog):
             embed.add_field(name=roles_list[item]["Role"] + " -  " + roles_list[item]["Emoji"] , value=roles_list[item]["Description"], inline=True)
 
         embed.set_footer(text=footer)
+
         menu = await ctx.send(embed=embed)
 
-        menu_id = menu.id
+        menu_id = str(menu.id)
+        
 
         for item in range(int(amount)):
             emoji = roles_list[item]["Emoji"]
+
             await menu.add_reaction(emoji)
+
+            await self.client.pg_con.execute("INSERT INTO bot_roles (guild_id, emoji_name, role_name, menu_id) VALUES ($1, $2, $3, $4)", 
+                                              guild_id, roles_list[item]["Emoji"], roles_list[item]["Role"], menu_id)
 
 
 def setup(client):
