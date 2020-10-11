1050a027ba1ce0edb1e95211d5f0e73a868293dc
Daniel <danielvenna2@gmail.com>
2020-10-11 12:33:56 -0300

    Added staticmethod to all functions in Security module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="moodleapi",
-    version="1.3.1",
+    version="1.3.2",
     author="Daniel Kauffmann",
     author_email="danielvenna2@gmail.com",
     description="MoodleAPI Package for Bot usable",
