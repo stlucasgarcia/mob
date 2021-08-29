d905404d4613d963a3208cc8c6232c3b35ad6092
Lucas Garcia <lsglucas@protonmail.com>
2021-08-29 12:43:28 -0300

    Added starter docs

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,28 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+
+export default defineUserConfig<DefaultThemeOptions>({
+	lang: "en-US",
+	title: "Mob Docs",
+	description: "Just playing around",
+
+	themeConfig: {
+		docsRepo: "lsglucas/mob",
+		docsBranch: "feature/docs",
+		navbar: [
+			{
+				text: "Guide",
+				link: "/guide/",
+			},
+			{
+				text: "API",
+				children: [
+					{ text: "Home", link: "/api/" },
+					{ text: "Contributing", link: "/api/contributing.md" },
+				],
+			},
+		],
+	},
+
+	bundler: "@vuepress/vite",
+});
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+lang: en-US
+title: Guide
+description: Documentation for Mob
+---
+
+# Guide
+
+Hey :wave: be patient, we`re still building this page.
